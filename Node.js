d7bf520051da277ec04d1e1047f0255d948ed6aa
Alex Lunyov <isuntc@gmail.com>
2018-04-30 13:00:29 +0800

    Inital node structure

---
+++ Node.js
@@ -0,0 +1,61 @@
+module.exports = class Node {
+  /*
+   * Returns current block hash
+   * @return String
+   */
+  async getCurrentBlock() {
+    return '0x0';
+  }
+
+  // ToDo: add block number support
+  /*
+   * Returns block object with transactions
+   * @param hash String
+   * @return Object
+   */
+  async getBlock(hash) {
+    return {
+      hash,
+      parentHash: '0x0',
+      size: 0,
+      transactions: [
+        {
+          hash: '0x0',
+          from: '0x0',
+          to: '0x0',
+          value: 0,
+          input: '0x0',
+        }
+      ],
+    };
+  }
+
+  /*
+   * Returns transaction object
+   * @param hash String
+   * @return Object
+   */
+  async getTransaction(hash) {
+    return {
+      hash,
+      from: '0x0',
+      to: '0x0',
+      value: 0,
+      input: '0x0',
+    };
+  }
+
+  /*
+   * Adds transaction to the current block
+   * @param tx Object
+   * @return String hash
+   */
+  async sendTransaction(tx) {
+    return '0x0';
+  }
+
+  /*
+   * Submits current block to the bridge
+   */
+  submitBlock() {}
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Node = require('./Node');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+const node = new Node();
+const app = express();
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+app.get('/block/:hash', async (req, res) => {
+  const block = await node.getBlock(req.params.hash);
+  res.send(block);
+});
+
+app.get('/tx/:hash', async (req, res) => {
+  const tx = await node.getTransaction(req.params.hash);
+  res.send(tx);
+});
+
+app.get('/currentBlock', async (req, res) => {
+  const hash = await node.getCurrentBlock();
+  res.send(hash);
+});
+
+app.post('/sendRawTransaction', async (req, res) => {
+  const hash = await node.getCurrentBlock(req.body.transaction);
+  res.send(hash);
+});
+
+// ToDo: listen contract events here
+
+const port = argv.port || argv.p || '8585';
+const host = argv.host || argv.h || '127.0.0.1';
+app.listen(
+  port,
+  host,
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Running on ${host}:${port}`);
+    }
+  }
+);
