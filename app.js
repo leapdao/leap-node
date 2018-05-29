97adf83c301e03a346f2a40cc5f6348214be6a14
Alex Lunyov <isuntc@gmail.com>
2018-05-29 16:19:22 +0800

    Updates for new tx structure

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const ethUtil = require('ethereumjs-util');
 const lotion = require('lotion');
 const { Tx, Type } = require('parsec-lib');
 
@@ -16,61 +17,73 @@ const app = lotion({
 const sumOuts = (value, out) => value + out.value;
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// Patched lotion with support of async handler needed here
-// https://github.com/parsec-labs/js-abci/commit/0d8ac8467b0e764b8b6f2c6f732501dc386dfcfc#diff-28f410b2e519d66e906cae6f42bcf5d8R12
 app.useTx(async (state, { encoded: rawTx }) => {
   await delay(200); // simulates network (contract calls, etc)
-  const tx = Tx.parse(rawTx);
+  const tx = Tx.fromRaw(rawTx);
   if (tx.type !== Type.DEPOSIT && tx.type !== Type.TRANSFER) {
     throw new Error('Unsupported tx type. Only deposits and transfers');
   }
 
-  if (state.txHashes.indexOf(tx.hash) > -1) {
+  if (state.txHashes.indexOf(tx.hash()) > -1) {
     throw new Error('Tx already submitted');
   }
 
   if (tx.type === Type.DEPOSIT) {
     // check deposit from contract here
-    const [{ addr, value }] = tx.outs;
-    state.balances[addr] = (state.balances[addr] || 0) + value;
-    state.unspent[tx.hash] = tx;
+    const [{ address, value }] = tx.outputs;
+    state.balances[address] = (state.balances[address] || 0) + value;
   }
 
   if (tx.type === Type.TRANSFER) {
-    const inputTransactions = tx.ins
-      .map(ins => ({ prevTx: state.unspent[ins.prevTx], outPos: ins.outPos }))
-      .filter(({ prevTx }) => !!prevTx);
+    const inputTransactions = tx.inputs
+      .map(input => ({
+        prevTx: state.unspent[ethUtil.bufferToHex(input.prevout.hash)],
+        outPos: input.prevout.index,
+      }))
+      .filter(({ prevTx, outPos }, i) => {
+        if (!prevTx) {
+          return false;
+        }
 
-    if (tx.ins.length !== inputTransactions.length) {
+        if (prevTx.outputs[outPos].address !== tx.inputs[i].signer) {
+          return false;
+        }
+
+        return true;
+      });
+
+    if (tx.inputs.length !== inputTransactions.length) {
       throw new Error('Wrong inputs');
     }
 
     const insValue = inputTransactions
-      .map(({ prevTx, outPos }) => prevTx.outs[outPos])
+      .map(({ prevTx, outPos }) => prevTx.outputs[outPos])
       .reduce(sumOuts, 0);
-    const outsValue = tx.outs.reduce(sumOuts, 0);
+    const outsValue = tx.outputs.reduce(sumOuts, 0);
 
     if (insValue !== outsValue) {
       throw new Error('Ins and outs values are mismatch');
     }
 
     inputTransactions.forEach(({ prevTx, outPos }) => {
-      const out = prevTx.outs[outPos];
-      state.balances[out.addr] -= out.value;
-      state.unspent[prevTx.hash].outs[outPos] = null;
+      const out = prevTx.outputs[outPos];
+      state.balances[out.address] -= out.value;
+      state.unspent[prevTx.hash].outputs[outPos] = null;
 
-      if (state.unspent[prevTx.hash].outs.filter(a => a).length === 0) {
+      if (state.unspent[prevTx.hash].outputs.filter(a => a).length === 0) {
         delete state.unspent[prevTx.hash];
       }
     });
 
-    tx.outs.forEach(out => {
-      state.balances[out.addr] = (state.balances[out.addr] || 0) + out.value;
+    tx.outputs.forEach(out => {
+      state.balances[out.address] =
+        (state.balances[out.address] || 0) + out.value;
     });
-    state.unspent[tx.hash] = tx;
   }
 
-  state.txHashes.push(tx.hash);
+  state.unspent[tx.hash()] = tx.toJSON();
+  state.txHashes.push(tx.hash());
+  console.log('state', state);
 });
 
 app.listen(process.env.PORT || 3000).then(params => console.log(params));
