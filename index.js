f6b2a3f9e57bcff46ff32743f177df96172246c2
Alex Lunyov <isuntc@gmail.com>
2018-06-06 16:50:50 +0800

    Generate ethereum private key for validator

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const { promisify } = require('util');
 const Web3 = require('web3');
 const { Tx } = require('parsec-lib');
 const lotion = require('lotion');
@@ -14,64 +18,91 @@ const validateTx = require('./src/validateTx');
 const accumulateTx = require('./src/accumulateTx');
 const validateBlock = require('./src/validateBlock');
 const eventsRelay = require('./src/eventsRelay');
+const { delay } = require('./src/utils');
 
 const config = require('./config.json');
 
+const writeFile = promisify(fs.writeFile);
+
 if (!config.bridgeAddr) {
-  console.error('bridgeAddr is required'); // eslint-disable-line
+  console.error('bridgeAddr is required');
   process.exit(0);
 }
 
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider(config.network));
+async function run() {
+  const web3 = new Web3();
+  web3.setProvider(new web3.providers.HttpProvider(config.network));
+  const bridge = new web3.eth.Contract(bridgeABI, config.bridgeAddr);
 
-const bridge = new web3.eth.Contract(bridgeABI, config.bridgeAddr);
+  if (!config.privKey) {
+    // ToDo: try to use private key from tendermint
+    const { privateKey } = web3.eth.accounts.create();
+    config.privKey = privateKey;
+    await writeFile('./config.json', JSON.stringify(config, null, 2));
+  }
 
-const app = lotion({
-  initialState: {
-    mempool: [],
-    balances: {}, // stores account balances
-    unspent: {}, // stores unspent outputs (deposits, transfers)
-  },
-  abciPort: 46658,
-});
+  const account = web3.eth.accounts.privateKeyToAccount(config.privKey);
 
-app.useInitializer(async state => {
-  /*
-   * Check if addr in validators list here (how? read all events?)
-   * ----------------------------------------------------------
-   * Join here if add is not a validator. How to set a stake?
-   * Maybe we should handle it outside the node.
-   * So validator should join somewhere (on join page)
-   * and after that run the node? Or init handler can wait for
-   * ValidatorJoin event with validator addr
-   * ----------------------------------------------------------
-   * How to update voting power in tendermint here?
-   * As an option, ValidatorJoin/ValidatorLeave events
-   * can be used to broadcast special tx (not from parsec-lib)
-   *
-   * Each node should check address from event with own
-   * address (we need private key?). If it's match, then
-   * node should update voting power of validator that run it.
-   *
-   * Voting power can be changed in useTx handler
-   * by mutating validators object in chainInfo param
-   *
-   * Validator address/pubKey (pubKey base64 using for validators object keys)
-   * can be found in $LOTION_PATH/config/priv_validator.json
-   */
-  console.log(state);
-});
+  const app = lotion({
+    initialState: {
+      mempool: [],
+      balances: {}, // stores account balances
+      unspent: {}, // stores unspent outputs (deposits, transfers)
+    },
+    abciPort: 46658,
+  });
 
-app.useTx(async (state, { encoded }) => {
-  const tx = Tx.fromRaw(encoded);
-  await validateTx(state, tx, bridge);
-  accumulateTx(state, tx);
-});
+  app.useInitializer(async () => {
+    const isValidator = false; // check if account.address in validators list
 
-app.useBlock(validateBlock);
+    if (!isValidator) {
+      console.log('=====');
+      console.log('You need to become a validator first');
+      console.log('Open http://localhost:/3001 and follow instruction');
+      console.log(`Your validator address will be: ${account.address}`);
+      console.log('=====');
 
-app.listen(config.port).then(params => {
-  console.log(params); // eslint-disable-line
-  eventsRelay(params.GCI, web3, bridge);
-});
+      await delay(2000); // waiting for join here
+    }
+    /*
+     * Check if addr in validators list here (how? read all events?)
+     * ----------------------------------------------------------
+     * Join here if add is not a validator. How to set a stake?
+     * Maybe we should handle it outside the node.
+     * So validator should join somewhere (on join page)
+     * and after that run the node? Or init handler can wait for
+     * ValidatorJoin event with validator addr
+     * ----------------------------------------------------------
+     * How to update voting power in tendermint here?
+     * As an option, ValidatorJoin/ValidatorLeave events
+     * can be used to broadcast special tx (not from parsec-lib)
+     *
+     * Each node should check address from event with own
+     * address (we need private key?). If it's match, then
+     * node should update voting power of validator that run it.
+     *
+     * Voting power can be changed in useTx handler
+     * by mutating validators object in chainInfo param
+     *
+     * Validator address/pubKey (pubKey base64 using for validators object keys)
+     * can be found in $LOTION_PATH/config/priv_validator.json
+     */
+  });
+
+  app.useTx(async (state, { encoded }) => {
+    const tx = Tx.fromRaw(encoded);
+    await validateTx(state, tx, bridge);
+    accumulateTx(state, tx);
+  });
+
+  app.useBlock(async (state, chainInfo) => {
+    await validateBlock(state, chainInfo, bridge, config.privKey);
+  });
+
+  app.listen(config.port).then(params => {
+    console.log(params);
+    eventsRelay(params.GCI, web3, bridge);
+  });
+}
+
+run();
