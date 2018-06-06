207378e64f44b2ea1f58242aa5d887bc09252ffe
Alex Lunyov <isuntc@gmail.com>
2018-06-06 14:31:00 +0800

    Extract eventsRelay module

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const Web3 = require('web3');
-const connect = require('lotion-connect');
-const { Tx, Input, Outpoint } = require('parsec-lib');
+const { Tx } = require('parsec-lib');
 const lotion = require('lotion');
 
 const bridgeABI = require('./src/bridgeABI');
-const ContractEventsSubscription = require('./src/ContractEventsSubscription');
 const validateTx = require('./src/validateTx');
 const validateBlock = require('./src/validateBlock');
-const { map } = require('./src/utils');
+const eventsRelay = require('./src/eventsRelay');
 
 const config = require('./config.json');
 
 if (!config.bridgeAddr) {
-  console.error('bridgeAddr is required');
+  console.error('bridgeAddr is required'); // eslint-disable-line
   process.exit(0);
 }
 
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider(config.network));
 
-// const bridgeAddr = '0xE5a9bDAFF671Dc0f9e32b6aa356E4D8938a49869';
 const bridge = new web3.eth.Contract(bridgeABI, config.bridgeAddr);
 
 const app = lotion({
@@ -36,37 +33,7 @@ app.useTx(async (state, { encoded }) => {
 
 app.useBlock(validateBlock);
 
-app.listen(config.port).then(async params => {
-  console.log(params);
-  const client = await connect(params.GCI);
-
-  const handleDeposit = async event => {
-    const deposit = await bridge.methods
-      .deposits(event.returnValues.depositId)
-      .call();
-    const tx = Tx.deposit(
-      event.returnValues.depositId,
-      Number(deposit.amount),
-      deposit.owner
-    );
-    await client.send({ encoded: tx.hex() });
-  };
-
-  const handleExit = async event => {
-    const { txHash, outIndex } = event.returnValues;
-    const tx = Tx.exit(new Input(new Outpoint(txHash, Number(outIndex))));
-    await client.send({ encoded: tx.hex() });
-  };
-
-  const eventSubscription = new ContractEventsSubscription(web3, bridge, 1000);
-  const {
-    NewDeposit: deposits = [],
-    ExitStarted: exits = [],
-  } = await eventSubscription.init();
-
-  await Promise.all(deposits.map(handleDeposit));
-  await Promise.all(exits.map(handleExit));
-
-  eventSubscription.on('NewDeposit', map(handleDeposit));
-  eventSubscription.on('ExitStarted', map(handleExit));
+app.listen(config.port).then(params => {
+  console.log(params); // eslint-disable-line
+  eventsRelay(params.GCI, web3, bridge);
 });
---
+++ src/eventsRelay.js
@@ -0,0 +1,39 @@
+const connect = require('lotion-connect');
+const { Tx, Input, Outpoint } = require('parsec-lib');
+
+const ContractEventsSubscription = require('./ContractEventsSubscription');
+const { map } = require('./utils');
+
+module.exports = async (GCI, web3, bridge) => {
+  const client = await connect(GCI);
+
+  const handleDeposit = async event => {
+    const deposit = await bridge.methods
+      .deposits(event.returnValues.depositId)
+      .call();
+    const tx = Tx.deposit(
+      event.returnValues.depositId,
+      Number(deposit.amount),
+      deposit.owner
+    );
+    await client.send({ encoded: tx.hex() });
+  };
+
+  const handleExit = async event => {
+    const { txHash, outIndex } = event.returnValues;
+    const tx = Tx.exit(new Input(new Outpoint(txHash, Number(outIndex))));
+    await client.send({ encoded: tx.hex() });
+  };
+
+  const eventSubscription = new ContractEventsSubscription(web3, bridge, 1000);
+  const {
+    NewDeposit: deposits = [],
+    ExitStarted: exits = [],
+  } = await eventSubscription.init();
+
+  await Promise.all(deposits.map(handleDeposit));
+  await Promise.all(exits.map(handleExit));
+
+  eventSubscription.on('NewDeposit', map(handleDeposit));
+  eventSubscription.on('ExitStarted', map(handleExit));
+};
