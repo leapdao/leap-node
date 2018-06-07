90ee9d17aeb8db41f8a1ceb0a5428b454d1cc859
Johann Barbie <johannbarbie@me.com>
2018-06-07 13:25:51 +0200

    add comments

--- src/checkBridge.js
+++ src/checkBridge.js
@@ -2,6 +2,10 @@ const { delay } = require('./utils');
 
 module.exports = async (rsp, chainInfo, height, { node }) => {
   console.log('checkBridge: ', chainInfo, node);
-  await delay(200); // simulates submit
+  // take the merkle root of prev_period
+  // look up if it has been submitted to the bridge already
+  await delay(200); // simulate reading from contract
+  // return 1 if found in bridge
+  // return 0 if not found in bridge, checkBridge will be called again in 5 secs
   rsp.status = 1;
 };
