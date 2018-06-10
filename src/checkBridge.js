22629f2aca7e55d9c6e86220b1d7008fc79ebf7c
Johann Barbie <johannbarbie@me.com>
2018-06-10 11:25:32 +0200

    new lotion version

--- src/checkBridge.js
+++ src/checkBridge.js
@@ -1,7 +1,7 @@
 const { delay } = require('./utils');
 
 module.exports = async (rsp, chainInfo, height, { node }) => {
-  console.log('checkBridge: ', chainInfo, node);
+  console.log('checkBridge: ', rsp, chainInfo, node);
   // take the merkle root of prev_period
   // look up if it has been submitted to the bridge already
   await delay(200); // simulate reading from contract
