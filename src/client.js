e7a5e8b62eea1b0fe3aefd15923ffd20e145ba10
Alex Lunyov <isuntc@gmail.com>
2018-06-18 16:30:41 +0800

    Add license headers

--- src/client.js
+++ src/client.js
@@ -1,6 +1,12 @@
+/**
+ * Copyright (c) 2018-present, Parsec Labs (parseclabs.org)
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
 const connect = require('lotion-connect');
 const Web3 = require('web3');
-const { Tx } = require('parsec-lib');
 const makeTransfer = require('./makeTransfer');
 
 const web3 = new Web3('https://rinkeby.infura.io');
@@ -8,7 +14,7 @@ const web3 = new Web3('https://rinkeby.infura.io');
 const privKey =
   '0xad8e31c8862f5f86459e7cca97ac9302c5e1817077902540779eef66e21f394a';
 const account = web3.eth.accounts.privateKeyToAccount(privKey);
-const GCI = '79d6d87c9b21d4aefed9dc2d716fa10d489ea4f298eb96c7cd7302535bf8a9cb';
+const GCI = '5758bf796dfb7f447c0d71fa7d75d8a3fd886636cb44d2904a41d0728f5a4af1';
 
 async function run() {
   const client = await connect(GCI);
@@ -17,18 +23,11 @@ async function run() {
   console.log(await client.state.balances);
   console.log('------');
 
-  const deposit = Tx.deposit(12, 5000, account.address);
-  await client.send({ encoded: deposit.hex() });
-  console.log('Deposit:', deposit.hex());
-  console.log('------');
-  console.log(await client.state.balances);
-  console.log('------');
-
   const transfer1 = await makeTransfer(
     client,
     account.address,
     '0x8AB21C65041778DFc7eC7995F9cDef3d5221a5ad',
-    4000,
+    1000,
     { privKey }
   );
   await client.send({ encoded: transfer1.hex() });
