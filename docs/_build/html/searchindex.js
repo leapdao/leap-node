1ba705db8d1036f232b5764b12c9c42051990d47
Kosta Korenkov <kosta@whoot.me>
2018-07-05 17:02:20 +0200

    docs: initial version for readthedocs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,19 @@
+Search.setIndex({
+  docnames: ['index'],
+  envversion: 52,
+  filenames: ['index.rst'],
+  objects: {},
+  objnames: {},
+  objtypes: {},
+  terms: { index: 0, modul: 0, page: 0, search: 0 },
+  titles: ['Welcome to PARSEC JSON RPC\u2019s documentation!'],
+  titleterms: {
+    document: 0,
+    indic: 0,
+    json: 0,
+    parsec: 0,
+    rpc: 0,
+    tabl: 0,
+    welcom: 0,
+  },
+});
