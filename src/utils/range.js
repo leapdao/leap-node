3b3359b37d2d0e162f67cfbfa5399cfbc0a16e4d
Kosta Korenkov <kosta@whoot.me>
2018-07-07 20:01:15 +0200

    add eth_call with balanceOf support for tokens

---
+++ src/utils/range.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2018-present, Parsec Labs (parseclabs.org)
+ *
+ * This source code is licensed under the Mozilla Public License Version 2.0
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = (s, e) => Array.from(new Array(e - s + 1), (_, i) => i + s);
