ce7c4c9af69bb32278ca9159c1646f65d702c7fe
Alex Lunyov <isuntc@gmail.com>
2018-07-07 18:38:49 +0200

    Update client. Ensure that everything works fine

--- src/utils/unspentForAddress.js
+++ src/utils/unspentForAddress.js
@@ -5,17 +5,18 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-module.exports = function unspentForAddress(unspent, address) {
+module.exports = function unspentForAddress(unspent, address, color) {
   return Object.keys(unspent)
     .map(k => ({
       outpoint: k,
       output: unspent[k],
     }))
-    .filter(u => {
-      return (
-        u.output && u.output.address.toLowerCase() === address.toLowerCase()
-      );
-    })
+    .filter(
+      u =>
+        u.output &&
+        u.output.address.toLowerCase() === address.toLowerCase() &&
+        u.output.color === color
+    )
     .sort((a, b) => {
       return a.output.value - b.output.value;
     });
