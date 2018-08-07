279d35e46870ee81f4e7e35a416ac3fc93b164c8
Alex Lunyov <isuntc@gmail.com>
2018-08-07 16:59:53 +0800

    Fix epoch update before epochLength tx

--- src/block/updateEpoch.js
+++ src/block/updateEpoch.js
@@ -1,6 +1,6 @@
 module.exports = (state, chainInfo) => {
   const heightFromLastEpoch = chainInfo.height - state.lastEpochHeight;
-  if (heightFromLastEpoch >= state.epochLength * 32) {
+  if (state.epochLength && heightFromLastEpoch >= state.epochLength * 32) {
     state.lastEpochHeight += state.epochLength * 32;
     state.epoch += 1;
   }
