229d3364969eb73d9f48d804afd23f7626a2732c
Alex Lunyov <isuntc@gmail.com>
2018-08-14 16:19:55 +0800

    Node → BridgeState

--- src/block/updatePeriod.js
+++ src/block/updatePeriod.js
@@ -12,47 +12,41 @@ const { getAuctionedByAddr } = require('../utils');
 const { logPeriod } = require('../debug');
 
 module.exports = async (state, chainInfo, bridgeState) => {
-  try {
-    if (chainInfo.height % 32 === 0) {
-      logPeriod('updatePeriod');
-      bridgeState.previousPeriod = bridgeState.currentPeriod;
-      bridgeState.currentPeriod = new Period(
-        bridgeState.previousPeriod.merkleRoot()
-      );
-      bridgeState.checkCallsCount = 0;
-      submitPeriod(
-        bridgeState.previousPeriod,
-        state.slots,
-        chainInfo.height,
-        bridgeState
-      );
-    }
-    if (chainInfo.height % 32 === 16) {
-      // check if there is a validator slot that is "waiting for me"
-      const myAuctionedSlots = getAuctionedByAddr(
-        state.slots,
-        bridgeState.account.address
-      )
-        .filter(
-          ({ activationEpoch }) => activationEpoch - state.epoch.epoch >= 2
-        )
-        .map(({ id }) => id);
-      if (myAuctionedSlots.length > 0) {
-        logPeriod('found some slots for activation', myAuctionedSlots);
-        myAuctionedSlots.forEach(id => {
-          const tx = activateSlot(id, bridgeState);
-          tx.catch(err => {
-            logPeriod('activation error', err.message);
-          });
-          if (typeof tx.on === 'function') {
-            tx.on('transactionHash', txHash => {
-              logPeriod('activate', id, txHash);
-            });
-          }
+  if (chainInfo.height % 32 === 0) {
+    logPeriod('updatePeriod');
+    bridgeState.previousPeriod = bridgeState.currentPeriod;
+    bridgeState.currentPeriod = new Period(
+      bridgeState.previousPeriod.merkleRoot()
+    );
+    bridgeState.checkCallsCount = 0;
+    submitPeriod(
+      bridgeState.previousPeriod,
+      state.slots,
+      chainInfo.height,
+      bridgeState
+    );
+  }
+  if (chainInfo.height % 32 === 16) {
+    // check if there is a validator slot that is "waiting for me"
+    const myAuctionedSlots = getAuctionedByAddr(
+      state.slots,
+      bridgeState.account.address
+    )
+      .filter(({ activationEpoch }) => activationEpoch - state.epoch.epoch >= 2)
+      .map(({ id }) => id);
+    if (myAuctionedSlots.length > 0) {
+      logPeriod('found some slots for activation', myAuctionedSlots);
+      myAuctionedSlots.forEach(id => {
+        const tx = activateSlot(id, bridgeState);
+        tx.catch(err => {
+          logPeriod('activation error', err.message);
         });
-      }
+        if (typeof tx.on === 'function') {
+          tx.on('transactionHash', txHash => {
+            logPeriod('activate', id, txHash);
+          });
+        }
+      });
     }
-  } catch (err) {
-    console.error(err);
   }
 };
--- src/period/index.js
+++ src/period/index.js
@@ -9,30 +9,26 @@ const submitPeriod = require('../txHelpers/submitPeriod');
 const { logPeriod } = require('../debug');
 
 module.exports = bridgeState => async (rsp, chainInfo) => {
-  try {
-    const height = chainInfo.height - 32;
-    if (height === 0) {
-      // genesis height doesn't need check
-      rsp.status = 1;
-      return;
-    }
-
-    logPeriod('checkBridge');
-    const contractPeriod = await submitPeriod(
-      bridgeState.previousPeriod,
-      bridgeState.currentState.slots,
-      height + bridgeState.checkCallsCount,
-      bridgeState
-    );
-
-    if (contractPeriod.timestamp === '0') {
-      rsp.status = 0;
-    } else {
-      rsp.status = 1;
-    }
-
-    bridgeState.checkCallsCount += 1;
-  } catch (err) {
-    console.error(err);
+  const height = chainInfo.height - 32;
+  if (height === 0) {
+    // genesis height doesn't need check
+    rsp.status = 1;
+    return;
   }
+
+  logPeriod('checkBridge');
+  const contractPeriod = await submitPeriod(
+    bridgeState.previousPeriod,
+    bridgeState.currentState.slots,
+    height + bridgeState.checkCallsCount,
+    bridgeState
+  );
+
+  if (contractPeriod.timestamp === '0') {
+    rsp.status = 0;
+  } else {
+    rsp.status = 1;
+  }
+
+  bridgeState.checkCallsCount += 1;
 };
