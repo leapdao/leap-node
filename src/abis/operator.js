28a855f810865e73bf9bfb89da20b9f6aaf38297
Alex Lunyov <isuntc@gmail.com>
2018-12-06 13:26:04 +0800

    Update operator ABI

--- src/abis/operator.js
+++ src/abis/operator.js
@@ -1,19 +1,54 @@
 module.exports = [
   {
     constant: true,
-    inputs: [{ name: '', type: 'uint256' }],
+    inputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
     name: 'slots',
     outputs: [
-      { name: 'eventCounter', type: 'uint32' },
-      { name: 'owner', type: 'address' },
-      { name: 'stake', type: 'uint64' },
-      { name: 'signer', type: 'address' },
-      { name: 'tendermint', type: 'bytes32' },
-      { name: 'activationEpoch', type: 'uint32' },
-      { name: 'newOwner', type: 'address' },
-      { name: 'newStake', type: 'uint64' },
-      { name: 'newSigner', type: 'address' },
-      { name: 'newTendermint', type: 'bytes32' },
+      {
+        name: 'eventCounter',
+        type: 'uint32',
+      },
+      {
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        name: 'stake',
+        type: 'uint64',
+      },
+      {
+        name: 'signer',
+        type: 'address',
+      },
+      {
+        name: 'tendermint',
+        type: 'bytes32',
+      },
+      {
+        name: 'activationEpoch',
+        type: 'uint32',
+      },
+      {
+        name: 'newOwner',
+        type: 'address',
+      },
+      {
+        name: 'newStake',
+        type: 'uint64',
+      },
+      {
+        name: 'newSigner',
+        type: 'address',
+      },
+      {
+        name: 'newTendermint',
+        type: 'bytes32',
+      },
     ],
     payable: false,
     stateMutability: 'view',
@@ -23,7 +58,12 @@ module.exports = [
     constant: true,
     inputs: [],
     name: 'lastCompleteEpoch',
-    outputs: [{ name: '', type: 'uint256' }],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
     payable: false,
     stateMutability: 'view',
     type: 'function',
@@ -32,7 +72,12 @@ module.exports = [
     constant: true,
     inputs: [],
     name: 'epochLength',
-    outputs: [{ name: '', type: 'uint256' }],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
     payable: false,
     stateMutability: 'view',
     type: 'function',
@@ -41,7 +86,12 @@ module.exports = [
     constant: true,
     inputs: [],
     name: 'lastEpochBlockHeight',
-    outputs: [{ name: '', type: 'uint256' }],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
     payable: false,
     stateMutability: 'view',
     type: 'function',
@@ -59,23 +109,57 @@ module.exports = [
     constant: true,
     inputs: [],
     name: 'owner',
-    outputs: [{ name: '', type: 'address' }],
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+    ],
     payable: false,
     stateMutability: 'view',
     type: 'function',
   },
   {
     constant: true,
-    inputs: [{ name: '', type: 'bytes32' }],
+    inputs: [
+      {
+        name: '',
+        type: 'bytes32',
+      },
+    ],
     name: 'periodData',
-    outputs: [{ name: 'slot', type: 'uint8' }],
+    outputs: [
+      {
+        name: 'slot',
+        type: 'uint8',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'bridge',
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+    ],
     payable: false,
     stateMutability: 'view',
     type: 'function',
   },
   {
     constant: false,
-    inputs: [{ name: '_newOwner', type: 'address' }],
+    inputs: [
+      {
+        name: '_newOwner',
+        type: 'address',
+      },
+    ],
     name: 'transferOwnership',
     outputs: [],
     payable: false,
@@ -84,8 +168,14 @@ module.exports = [
   },
   {
     inputs: [
-      { name: '_bridge', type: 'address' },
-      { name: '_epochLength', type: 'uint256' },
+      {
+        name: '_bridge',
+        type: 'address',
+      },
+      {
+        name: '_epochLength',
+        type: 'uint256',
+      },
     ],
     payable: false,
     stateMutability: 'nonpayable',
@@ -93,18 +183,56 @@ module.exports = [
   },
   {
     anonymous: false,
-    inputs: [{ indexed: false, name: 'epoch', type: 'uint256' }],
+    inputs: [
+      {
+        indexed: false,
+        name: 'epoch',
+        type: 'uint256',
+      },
+    ],
     name: 'Epoch',
     type: 'event',
   },
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name: 'signerAddr', type: 'address' },
-      { indexed: true, name: 'slotId', type: 'uint256' },
-      { indexed: true, name: 'tenderAddr', type: 'bytes32' },
-      { indexed: false, name: 'eventCounter', type: 'uint256' },
-      { indexed: false, name: 'epoch', type: 'uint256' },
+      {
+        indexed: false,
+        name: 'epoch',
+        type: 'uint256',
+      },
+    ],
+    name: 'EpochLength',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'signerAddr',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'slotId',
+        type: 'uint256',
+      },
+      {
+        indexed: true,
+        name: 'tenderAddr',
+        type: 'bytes32',
+      },
+      {
+        indexed: false,
+        name: 'eventCounter',
+        type: 'uint256',
+      },
+      {
+        indexed: false,
+        name: 'epoch',
+        type: 'uint256',
+      },
     ],
     name: 'ValidatorJoin',
     type: 'event',
@@ -112,12 +240,36 @@ module.exports = [
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name: 'signerAddr', type: 'address' },
-      { indexed: true, name: 'slotId', type: 'uint256' },
-      { indexed: true, name: 'tenderAddr', type: 'bytes32' },
-      { indexed: false, name: 'newSigner', type: 'address' },
-      { indexed: false, name: 'eventCounter', type: 'uint256' },
-      { indexed: false, name: 'epoch', type: 'uint256' },
+      {
+        indexed: true,
+        name: 'signerAddr',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'slotId',
+        type: 'uint256',
+      },
+      {
+        indexed: true,
+        name: 'tenderAddr',
+        type: 'bytes32',
+      },
+      {
+        indexed: false,
+        name: 'newSigner',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        name: 'eventCounter',
+        type: 'uint256',
+      },
+      {
+        indexed: false,
+        name: 'epoch',
+        type: 'uint256',
+      },
     ],
     name: 'ValidatorLogout',
     type: 'event',
@@ -125,10 +277,26 @@ module.exports = [
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name: 'signerAddr', type: 'address' },
-      { indexed: true, name: 'slotId', type: 'uint256' },
-      { indexed: true, name: 'tenderAddr', type: 'bytes32' },
-      { indexed: false, name: 'epoch', type: 'uint256' },
+      {
+        indexed: true,
+        name: 'signerAddr',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'slotId',
+        type: 'uint256',
+      },
+      {
+        indexed: true,
+        name: 'tenderAddr',
+        type: 'bytes32',
+      },
+      {
+        indexed: false,
+        name: 'epoch',
+        type: 'uint256',
+      },
     ],
     name: 'ValidatorLeave',
     type: 'event',
@@ -136,32 +304,67 @@ module.exports = [
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name: 'signerAddr', type: 'address' },
-      { indexed: true, name: 'slotId', type: 'uint256' },
-      { indexed: true, name: 'tenderAddr', type: 'bytes32' },
-      { indexed: false, name: 'eventCounter', type: 'uint256' },
+      {
+        indexed: true,
+        name: 'signerAddr',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'slotId',
+        type: 'uint256',
+      },
+      {
+        indexed: true,
+        name: 'tenderAddr',
+        type: 'bytes32',
+      },
+      {
+        indexed: false,
+        name: 'eventCounter',
+        type: 'uint256',
+      },
     ],
     name: 'ValidatorUpdate',
     type: 'event',
   },
   {
     anonymous: false,
-    inputs: [{ indexed: true, name: 'previousOwner', type: 'address' }],
+    inputs: [
+      {
+        indexed: true,
+        name: 'previousOwner',
+        type: 'address',
+      },
+    ],
     name: 'OwnershipRenounced',
     type: 'event',
   },
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name: 'previousOwner', type: 'address' },
-      { indexed: true, name: 'newOwner', type: 'address' },
+      {
+        indexed: true,
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'newOwner',
+        type: 'address',
+      },
     ],
     name: 'OwnershipTransferred',
     type: 'event',
   },
   {
     constant: false,
-    inputs: [{ name: '_epochLength', type: 'uint256' }],
+    inputs: [
+      {
+        name: '_epochLength',
+        type: 'uint256',
+      },
+    ],
     name: 'setEpochLength',
     outputs: [],
     payable: false,
@@ -171,10 +374,22 @@ module.exports = [
   {
     constant: false,
     inputs: [
-      { name: '_slotId', type: 'uint256' },
-      { name: '_value', type: 'uint256' },
-      { name: '_signerAddr', type: 'address' },
-      { name: '_tenderAddr', type: 'bytes32' },
+      {
+        name: '_slotId',
+        type: 'uint256',
+      },
+      {
+        name: '_value',
+        type: 'uint256',
+      },
+      {
+        name: '_signerAddr',
+        type: 'address',
+      },
+      {
+        name: '_tenderAddr',
+        type: 'bytes32',
+      },
     ],
     name: 'bet',
     outputs: [],
@@ -184,7 +399,12 @@ module.exports = [
   },
   {
     constant: false,
-    inputs: [{ name: '_slotId', type: 'uint256' }],
+    inputs: [
+      {
+        name: '_slotId',
+        type: 'uint256',
+      },
+    ],
     name: 'activate',
     outputs: [],
     payable: false,
@@ -194,9 +414,18 @@ module.exports = [
   {
     constant: false,
     inputs: [
-      { name: '_slotId', type: 'uint256' },
-      { name: '_prevHash', type: 'bytes32' },
-      { name: '_root', type: 'bytes32' },
+      {
+        name: '_slotId',
+        type: 'uint256',
+      },
+      {
+        name: '_prevHash',
+        type: 'bytes32',
+      },
+      {
+        name: '_root',
+        type: 'bytes32',
+      },
     ],
     name: 'submitPeriod',
     outputs: [],
