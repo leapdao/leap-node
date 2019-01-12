010c0d8dd276fda1d6bca9c2f97c595eb85544a3
Kosta Korenkov <kosta@whoot.me>
2019-01-12 21:45:21 +0300

    small cleanup

--- terraform_node.sh
+++
@@ -1,2 +0,0 @@
-terraform init setup/cloud
-terraform apply -var ssh_public_file="~/.ssh/leap-testnet.pub" -var ssh_private_file="~/.ssh/leap-testnet" -var network="testnet-gamma" -var count=$1 setup/cloud
