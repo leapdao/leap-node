a72528157b157b51fe7902e7972ac7da0fa3b332
Johann Barbie <johannbarbie@me.com>
2019-01-24 09:56:16 +0100

    add test for buffer

--- src/api/methods/sendRawTransaction.test.js
+++ src/api/methods/sendRawTransaction.test.js
@@ -7,7 +7,7 @@ const A1 = '0xB8205608d54cb81f44F263bE086027D8610F3C94';
 jest.mock('axios');
 
 describe('sendRawTransaction', () => {
-  test('success', async () => {
+  test('success with hex', async () => {
     axios.post.mockImplementation(() => {
       return true;
     });
@@ -15,6 +15,16 @@ describe('sendRawTransaction', () => {
     const response = await sendRawTransaction(0, tx.hex());
     expect(response).toBe(tx.hash());
   });
+
+  test('success with buffer', async () => {
+    axios.post.mockImplementation(() => {
+      return true;
+    });
+    const tx = Tx.deposit(0, 100, A1, 0);
+    const response = await sendRawTransaction(0, tx.toRaw());
+    expect(response).toBe(tx.hash());
+  });
+
   test('error', async () => {
     const error = new Error();
     axios.post.mockImplementation(() => {
