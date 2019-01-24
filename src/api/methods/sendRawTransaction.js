fbb73490bc4db5233ee1da8327b15d2e631da6a6
Johann Barbie <johannbarbie@users.noreply.github.com>
2019-01-24 11:52:44 +0100

    follow RPC specification (#154)
    * follow RPC specification
    
    * let jsonrpc server catch errors
    
    * add test for buffer

--- src/api/methods/sendRawTransaction.js
+++ src/api/methods/sendRawTransaction.js
@@ -2,8 +2,7 @@ const { Tx } = require('leap-core');
 const sendTx = require('../../txHelpers/sendTx');
 
 module.exports = async (lotionPort, rawTx) => {
-  const data = Buffer.from(rawTx.data.replace('0x', ''), 'hex');
-  const tx = Tx.fromRaw(data);
-  await sendTx(lotionPort, `0x${data.toString('hex')}`);
+  const tx = Tx.fromRaw(rawTx);
+  await sendTx(lotionPort, rawTx);
   return tx.hash();
 };
--- src/api/methods/sendRawTransaction.test.js
+++ src/api/methods/sendRawTransaction.test.js
@@ -1,14 +1,40 @@
+const axios = require('axios');
 const { Tx } = require('leap-core');
 const sendRawTransaction = require('./sendRawTransaction');
 
 const A1 = '0xB8205608d54cb81f44F263bE086027D8610F3C94';
 
-jest.mock('../../txHelpers/sendTx');
+jest.mock('axios');
 
 describe('sendRawTransaction', () => {
-  test('success', async () => {
+  test('success with hex', async () => {
+    axios.post.mockImplementation(() => {
+      return true;
+    });
     const tx = Tx.deposit(0, 100, A1, 0);
-    const response = await sendRawTransaction(0, { data: tx.hex() });
+    const response = await sendRawTransaction(0, tx.hex());
     expect(response).toBe(tx.hash());
+  });
+
+  test('success with buffer', async () => {
+    axios.post.mockImplementation(() => {
+      return true;
+    });
+    const tx = Tx.deposit(0, 100, A1, 0);
+    const response = await sendRawTransaction(0, tx.toRaw());
+    expect(response).toBe(tx.hash());
+  });
+
+  test('error', async () => {
+    const error = new Error();
+    axios.post.mockImplementation(() => {
+      throw error;
+    });
+    const tx = Tx.deposit(0, 100, A1, 0);
+    try {
+      await sendRawTransaction(0, tx.hex());
+    } catch (err) {
+      expect(err).toBe(error);
+    }
   });
 });
