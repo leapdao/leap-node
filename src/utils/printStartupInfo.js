9724f59e963e8b2e5f6d873a6eb62c99e87002c7
Alex Lunyov <isuntc@gmail.com>
2019-02-20 22:42:44 +0800

    Do not print PoS related info on startup

--- src/utils/printStartupInfo.js
+++ src/utils/printStartupInfo.js
@@ -7,62 +7,61 @@
 
 /* eslint-disable no-console */
 
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
-const colors = require('colors');
-const getSlotsByAddr = require('./getSlotsByAddr');
-const readSlots = require('./readSlots');
+// const { promisify } = require('util');
+// const fs = require('fs');
+// const path = require('path');
+// const colors = require('colors');
+// const getSlotsByAddr = require('./getSlotsByAddr');
+// const readSlots = require('./readSlots');
+// const readFile = promisify(fs.readFile);
 const { logNode } = require('../utils/debug');
-
-const readFile = promisify(fs.readFile);
 
 module.exports = async (params, bridgeState) => {
   logNode(`Last block synced: ${bridgeState.lastBlockSynced}`);
 
-  const validatorKeyPath = path.join(
-    params.lotionPath,
-    'config',
-    'priv_validator.json'
-  );
+  // const validatorKeyPath = path.join(
+  //   params.lotionPath,
+  //   'config',
+  //   'priv_validator.json'
+  // );
 
-  const validatorKey = JSON.parse(await readFile(validatorKeyPath, 'utf-8'));
-  const validatorID = Buffer.from(
-    validatorKey.pub_key.value,
-    'base64'
-  ).toString('hex');
-  const slots = await readSlots(bridgeState.operatorContract);
-  const mySlots = getSlotsByAddr(slots, bridgeState.account.address);
+  // const validatorKey = JSON.parse(await readFile(validatorKeyPath, 'utf-8'));
+  // const validatorID = Buffer.from(
+  //   validatorKey.pub_key.value,
+  //   'base64'
+  // ).toString('hex');
+  // const slots = await readSlots(bridgeState.operatorContract);
+  // const mySlots = getSlotsByAddr(slots, bridgeState.account.address);
 
-  mySlots.forEach(slot => {
-    if (
-      slot.tenderKey.replace('0x', '').toLowerCase() !==
-      validatorID.toLowerCase()
-    ) {
-      console.log(
-        `You need to update validator ID in slot ${slot.id} to ${validatorID}`
-      );
-    }
-  });
+  // mySlots.forEach(slot => {
+  //   if (
+  //     slot.tenderKey.replace('0x', '').toLowerCase() !==
+  //     validatorID.toLowerCase()
+  //   ) {
+  //     console.log(
+  //       `You need to update validator ID in slot ${slot.id} to ${validatorID}`
+  //     );
+  //   }
+  // });
 
-  if (mySlots.length === 0) {
-    console.log('\n');
-    console.log(
-      `  ${'You need to become a validator first'.underline.bold.red}`
-    );
-    console.log(
-      `  Open ${colors.bold(
-        `https://bridge-dev.leapdao.org/${
-          bridgeState.bridgeContract.options.address
-        }`
-      )} and buy a slot`
-    );
-    console.log(
-      `  ${colors.bold('Validator address:')}\t${bridgeState.account.address}`
-    );
-    console.log(`  ${colors.bold('Validator ID:')}\t\t${validatorID}`);
-    console.log('\n');
-  }
+  // if (mySlots.length === 0) {
+  //   console.log('\n');
+  //   console.log(
+  //     `  ${'You need to become a validator first'.underline.bold.red}`
+  //   );
+  //   console.log(
+  //     `  Open ${colors.bold(
+  //       `https://bridge-dev.leapdao.org/${
+  //         bridgeState.bridgeContract.options.address
+  //       }`
+  //     )} and buy a slot`
+  //   );
+  //   console.log(
+  //     `  ${colors.bold('Validator address:')}\t${bridgeState.account.address}`
+  //   );
+  //   console.log(`  ${colors.bold('Validator ID:')}\t\t${validatorID}`);
+  //   console.log('\n');
+  // }
 
   logNode(
     `Network: ${bridgeState.config.network}, Network ID: ${
