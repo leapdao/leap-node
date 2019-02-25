9fead251a8e119e2535c017263c9e6682e80cdf4
Kosta Korenkov <kosta@whoot.me>
2019-02-25 19:52:03 +0300

    ignore gas price api for non-mainnet networks

--- src/utils/getRootGasPrice.js
+++ src/utils/getRootGasPrice.js
@@ -10,11 +10,15 @@ const axios = require('axios');
 const GAS_STATION_API = 'https://ethgasstation.info/json/ethgasAPI.json';
 
 /**
- * Reads proposed gas price from the ethgasstation API.
+ * Reads proposed gas price from the ethgasstation API for mainnet. For other
+ * networks returns `null`.
+ *
  * Default price urgency is "fast" (block in < 2 min).
  * Other options are: safeLow, average, fastest
  */
-module.exports = async function getRootGasPrice(urgency = 'fast') {
+module.exports = async function getRootGasPrice(web3, urgency = 'fast') {
+  const networkId = await web3.eth.net.getId();
+  if (networkId !== 1) return null;
   return axios.get(GAS_STATION_API).then(response => {
     return (response.data[urgency] / 10) * 10 ** 9;
   });
--- src/utils/getRootGasPrice.test.js
+++ src/utils/getRootGasPrice.test.js
@@ -1,8 +1,14 @@
+const Web3 = require('web3');
 const getRootGasPrice = require('./getRootGasPrice');
 
 describe('getRootGasPrice', () => {
-  test('Reads gas price from gas station API', async () => {
-    const result = await getRootGasPrice();
+  test('Reads gas price from gas station API for mainnet', async () => {
+    const result = await getRootGasPrice(new Web3('https://mainnet.infura.io'));
     expect(result).toBeGreaterThan(0);
+  });
+
+  test('Returns null for non-mainnet networks', async () => {
+    const result = await getRootGasPrice(new Web3('https://rinkeby.infura.io'));
+    expect(result).toBeNull();
   });
 });
