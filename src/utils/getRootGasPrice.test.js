333e58ec17812aa8fe7f87a81a08dd26d96c797d
Kosta Korenkov <kosta@whoot.me>
2019-02-26 13:15:54 +0300

    tests: check non-default gas price setting

--- src/utils/getRootGasPrice.test.js
+++ src/utils/getRootGasPrice.test.js
@@ -1,10 +1,38 @@
 const Web3 = require('web3');
+const axios = require('axios');
 const getRootGasPrice = require('./getRootGasPrice');
 
+jest.mock('axios');
+
+axios.get.mockResolvedValue({
+  data: {
+    fast: 200.0,
+    fastest: 270.0,
+    safeLow: 134.0,
+    average: 180.0,
+    block_time: 19.228571428571428,
+    blockNum: 7269705,
+    speed: 0.998286333650524,
+    safeLowWait: 22.6,
+    avgWait: 3.2,
+    fastWait: 0.6,
+    fastestWait: 0.6,
+  },
+});
+
 describe('getRootGasPrice', () => {
-  test('Reads gas price from gas station API for mainnet', async () => {
-    const result = await getRootGasPrice(new Web3('https://mainnet.infura.io'));
-    expect(result).toBeGreaterThan(0);
+  describe('mainnet', () => {
+    const web3 = new Web3('https://mainnet.infura.io');
+
+    test('Reads "fast" gas price by default from gas station API', async () => {
+      const result = await getRootGasPrice(web3);
+      expect(result).toBe(20000000000);
+    });
+
+    test('Reads specified gas price from gas station API', async () => {
+      const result = await getRootGasPrice(web3, 'safeLow');
+      expect(result).toBe(13400000000);
+    });
   });
 
   test('Returns null for non-mainnet networks', async () => {
