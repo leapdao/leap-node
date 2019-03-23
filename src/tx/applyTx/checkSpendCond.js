ba49b4000e6185bb4bf0174de14f59df35d1f6f3
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-03-23 10:40:50 +0100

    Upgrade spending conditions to petersburg (#215)
    Fixes #199

--- src/tx/applyTx/checkSpendCond.js
+++ src/tx/applyTx/checkSpendCond.js
@@ -6,9 +6,7 @@
  */
 
 const { Type, Output } = require('leap-core');
-const Account = require('ethereumjs-account');
 const Transaction = require('ethereumjs-tx');
-const Trie = require('merkle-patricia-tree');
 const VM = require('ethereumjs-vm');
 const utils = require('ethereumjs-util');
 const { BigInt, add, divide, subtract, lessThan } = require('jsbi-utils');
@@ -17,35 +15,42 @@ const { checkInsAndOuts, groupValuesByColor } = require('./utils');
 const getColors = require('../../api/methods/getColors');
 const { NFT_COLOR_BASE } = require('../../api/methods/constants');
 
+const { Account } = VM.deps;
+
 // commpiled https://github.com/leapdao/spending-conditions/blob/master/contracts/ERC20Min.sol
 const erc20Code = Buffer.from(
   '608060405234801561001057600080fd5b506004361061005d577c0100000000000000000000000000000000000000000000000000000000600035046340c10f19811461006257806370a08231146100a2578063a9059cbb146100da575b600080fd5b61008e6004803603604081101561007857600080fd5b50600160a060020a038135169060200135610106565b604080519115158252519081900360200190f35b6100c8600480360360208110156100b857600080fd5b5035600160a060020a0316610128565b60408051918252519081900360200190f35b61008e600480360360408110156100f057600080fd5b50600160a060020a038135169060200135610143565b60006001331461011557600080fd5b61011f8383610150565b50600192915050565b600160a060020a031660009081526020819052604090205490565b600061011f3384846101e4565b600160a060020a038216151561016557600080fd5b600160a060020a03821660009081526020819052604090205461018e908263ffffffff6102b116565b600160a060020a0383166000818152602081815260408083209490945583518581529351929391927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9281900390910190a35050565b600160a060020a03821615156101f957600080fd5b600160a060020a038316600090815260208190526040902054610222908263ffffffff6102ca16565b600160a060020a038085166000908152602081905260408082209390935590841681522054610257908263ffffffff6102b116565b600160a060020a038084166000818152602081815260409182902094909455805185815290519193928716927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef92918290030190a3505050565b6000828201838110156102c357600080fd5b9392505050565b6000828211156102d957600080fd5b5090039056fea165627a7a72305820b48eed6297042d728d0fddfa2756bf34e6a1faa2965516c2d03dbfc53e01065d0029',
   'hex'
 );
+
 const REACTOR_ADDR = Buffer.from(
   '0000000000000000000000000000000000000001',
   'hex'
 );
 
-function setAccount(account, trie, address) {
-  // store in the trie
+const MINT_FUNCSIG = Buffer.from(
+  '40c10f19000000000000000000000000',
+  'hex'
+);
+
+function setAccount(account, address, stateManager) {
   return new Promise((resolve, reject) => {
-    trie.put(address, account.serialize(), (err, val) => {
+    stateManager.putAccount(address, account, (err) => {
       if (err) {
         return reject(err);
       }
-      return resolve(val);
+      return resolve();
     });
   });
 }
 
-function setAccountCode(account, trie, code) {
+function setAccountCode(code, address, stateManager) {
   return new Promise((resolve, reject) => {
-    account.setCode(trie, code, (err, codeHash) => {
+    stateManager.putContractCode(address, code, (err) => {
       if (err) {
         return reject(err);
       }
-      return resolve(codeHash);
+      return resolve();
     });
   });
 }
@@ -112,26 +117,24 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
       state.unspent[tx.inputs[i].prevout.hex()];
   }
 
-  // creating a trie that just resides in memory
-  const stateTrie = new Trie();
   // creating a new VM instance
-  const vm = new VM({ state: stateTrie });
+  const vm = new VM({ hardfork: 'petersburg' });
   // creating the reactor account with some wei
   const reactorAccount = new Account();
   reactorAccount.balance = '0xf00000000000000001';
-  await setAccount(reactorAccount, stateTrie, REACTOR_ADDR);
+  await setAccount(reactorAccount, REACTOR_ADDR, vm.stateManager);
 
   // deploying colors and mint tokens
   let results;
   let nonceCounter = 0;
+
+  const sigHashBuf = tx.sigHashBuf();
   const insValues = Object.values(inputMap).reduce(groupValuesByColor, {});
   const outsValues = tx.outputs.reduce(groupValuesByColor, {});
   // eslint-disable-next-line  guard-for-in
   for (const color in insValues) {
-    const erc20Account = new Account();
-    await setAccountCode(erc20Account, stateTrie, erc20Code); // eslint-disable-line no-await-in-loop
     // eslint-disable-next-line no-await-in-loop
-    await setAccount(erc20Account, stateTrie, colorMap[color]);
+    await setAccountCode(erc20Code, colorMap[color], vm.stateManager); // eslint-disable-line no-await-in-loop
 
     // minting amount of output to address of condition
     const amountHex = utils.setLengthLeft(
@@ -143,19 +146,24 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
       nonce: nonceCounter,
       gasLimit: '0xffffffffffff',
       to: colorMap[color],
-      data: `0x40c10f19000000000000000000000000${tx
-        .sigHash()
-        .replace('0x', '')}${amountHex.toString('hex')}`,
+      data: Buffer.concat([MINT_FUNCSIG, sigHashBuf, amountHex]),
     });
     nonceCounter += 1;
   }
 
   // deploying conditions
   tx.inputs.forEach(async input => {
-    const conditionAccount = new Account();
-    await setAccountCode(conditionAccount, stateTrie, input.script);
     // TODO: what if there are multiple condition scripts in one tx?
-    await setAccount(conditionAccount, stateTrie, tx.sigHash());
+    await setAccountCode(input.script, sigHashBuf, vm.stateManager);
+  });
+
+  // need to commit to trie, needs a checkpoint first 🤪
+  await new Promise((resolve) => {
+    vm.stateManager.checkpoint(() => {
+      vm.stateManager.commit(() => {
+        resolve();
+      });
+    });
   });
 
   const logOuts = [];
@@ -166,7 +174,7 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
     results = await runTx(vm, {
       nonce: nonceCounter,
       gasLimit: 6000000, // TODO: set gas Limit to (inputs - outputs) / gasPrice
-      to: tx.sigHash(), // the plasma address is replaced with sighash, to prevent replay attacks
+      to: sigHashBuf, // the plasma address is replaced with sighash, to prevent replay attacks
       data: tx.inputs[i].msgData,
     });
     nonceCounter += 1;
@@ -186,12 +194,12 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
     results.vm.logs.forEach(log => {
       if (log[0].equals(colorMap[out.color])) {
         const transferAmount = BigInt(`0x${log[2].toString('hex')}`, 16);
-        let toAddr = `0x${log[1][2].slice(12, 32).toString('hex')}`;
+        let toAddr = log[1][2].slice(12, 32);
         // replace injected sigHash with plasma address
-        if (toAddr === tx.sigHash()) {
-          toAddr = `0x${utils.ripemd160(tx.inputs[i].script).toString('hex')}`;
+        if (toAddr.equals(sigHashBuf)) {
+          toAddr = utils.ripemd160(tx.inputs[i].script);
         }
-        logOuts.push(new Output(transferAmount, toAddr, out.color));
+        logOuts.push(new Output(transferAmount, `0x${toAddr.toString('hex')}`, out.color));
       }
     });
   }
