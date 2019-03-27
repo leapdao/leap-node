40ca1702a845c11e916fa97ad3379398e8b52153
Kosta Korenkov <kosta@whoot.me>
2019-03-27 16:17:47 +0300

    chore(cli): no need to fiddle with priv_validator_key.json on --fresh
    the file is immutable in latest tendermints

--- src/utils/cleanupLotion.js
+++ src/utils/cleanupLotion.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const rimraf = promisify(require('rimraf'));
 
-const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const exists = promisify(fs.exists);
 
@@ -16,21 +15,6 @@ module.exports = async app => {
   if (await exists(lotionPath)) {
     const configPath = path.join(lotionPath, 'config');
     const dataPath = path.join(lotionPath, 'data');
-    const privValidatorPath = path.join(configPath, 'priv_validator_key.json');
-    const privValidator = JSON.parse(await readFile(privValidatorPath));
-    await writeFile(
-      privValidatorPath,
-      JSON.stringify(
-        {
-          address: privValidator.address,
-          pub_key: privValidator.pub_key,
-          priv_key: privValidator.priv_key,
-        },
-        null,
-        2
-      )
-    );
-
     await rimraf(dataPath);
     await rimraf(path.join(lotionPath, 'merk'));
     await rimraf(path.join(lotionPath, 'leap.db'));
