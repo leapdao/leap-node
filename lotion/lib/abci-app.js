ed5f534b1b1edb97429bebf75c4ff55927c2bd60
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-03-29 00:22:36 +0100

    Next breaking changes for leap-node
    - Remove Tx encoding in abci, it's useless ***BREAKING CHANGE***

--- lotion/lib/abci-app.js
+++ lotion/lib/abci-app.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-await-in-loop */
 
 const createABCIServer = require('js-abci');
-const decodeTx = require('./tx-encoding.js').decode;
 const jsondiffpatch = require('jsondiffpatch');
 
 const { getAddress } = require('../../src/utils');
@@ -116,11 +115,10 @@ module.exports = function configureABCIServer({
   abciApp.checkTx = async req => {
     const rawTx = req.tx;
     try {
-      const tx = decodeTx(rawTx);
       const [isValid, log] = await runTx(
         txMiddleware,
         store,
-        tx,
+        rawTx,
         chainInfo,
         false
       );
@@ -134,11 +132,10 @@ module.exports = function configureABCIServer({
   abciApp.deliverTx = async req => {
     const rawTx = req.tx;
     try {
-      const tx = decodeTx(rawTx);
       const [isValid, log] = await runTx(
         txMiddleware,
         store,
-        tx,
+        rawTx,
         chainInfo,
         true
       );
