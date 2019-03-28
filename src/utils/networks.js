a3aa2d8058f2bea75fd48806e4670f50cb7feedd
Kosta Korenkov <kosta@whoot.me>
2019-03-28 15:44:04 +0300

    fix(bridgeState): use v3 infura URLs for root network provider (#220)

--- src/utils/networks.js
+++ src/utils/networks.js
@@ -5,37 +5,61 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-const NETWORKS = {
+module.exports = {
   1: {
     name: 'Mainnet',
-    provider: 'https://mainnet.infura.io',
+    provider: {
+      http: 'https://mainnet.infura.io/v3/f039330d8fb747e48a7ce98f51400d65',
+      ws: 'wss://mainnet.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65',
+    },
     etherscanBase: 'https://etherscan.io',
   },
   3: {
     name: 'Ropsten',
-    provider: 'https://ropsten.infura.io',
+    provider: {
+      http: 'https://ropsten.infura.io/v3/f039330d8fb747e48a7ce98f51400d65',
+      ws: 'wss://ropsten.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65',
+    },
     etherscanBase: 'https://ropsten.etherscan.io',
   },
   4: {
     name: 'Rinkeby',
-    provider: 'https://rinkeby.infura.io',
+    provider: {
+      http: 'https://rinkeby.infura.io/v3/f039330d8fb747e48a7ce98f51400d65',
+      ws: 'wss://rinkeby.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65',
+    },
     etherscanBase: 'https://rinkeby.etherscan.io',
+  },
+  5: {
+    name: 'Görli',
+    provider: {
+      http: 'https://goerli.infura.io/v3/f039330d8fb747e48a7ce98f51400d65',
+      ws: 'wss://goerli.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65',
+    },
+    etherscanBase: 'https://goerli.etherscan.io',
   },
   42: {
     name: 'Kovan',
-    provider: 'https://kovan.infura.io',
+    provider: {
+      http: 'https://kovan.infura.io/v3/f039330d8fb747e48a7ce98f51400d65',
+      ws: 'wss://kovan.infura.io/ws/v3/f039330d8fb747e48a7ce98f51400d65',
+    },
     etherscanBase: 'https://kovan.etherscan.io',
   },
   4447: {
     name: 'Truffle',
-    provider: 'http://localhost:9545',
+    provider: {
+      http: 'http://localhost:9545',
+      ws: 'ws://localhost:9545',
+    },
     etherscanBase: 'https://etherscan.io',
   },
   5777: {
     name: 'Ganache',
-    provider: 'http://localhost:8545',
+    provider: {
+      http: 'http://localhost:8545',
+      ws: 'ws://localhost:8545',
+    },
     etherscanBase: 'https://etherscan.io',
   },
 };
-
-module.exports = NETWORKS;
