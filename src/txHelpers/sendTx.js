5176a97b0705f9dff794e14e35b29aefb038a7c6
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-03-29 00:22:36 +0100

    sendTx: use broadcast_tx_sync..
    ..instead of broadcast_tx_commit.
    
    We only should check if the tx is valid (passes mempool), but we don't have to
    wait until it is included in a block;

--- src/txHelpers/sendTx.js
+++ src/txHelpers/sendTx.js
@@ -11,7 +11,7 @@ const encodeTx = require('../../lotion/lib/tx-encoding.js').encode;
 module.exports = async (tendermintPort, rawTx) => {
   const nonce = Math.floor(Math.random() * (2 << 12)); // eslint-disable-line no-bitwise
   const txBytes = `0x${encodeTx({ encoded: rawTx }, nonce).toString('hex')}`;
-  const tendermintRpcUrl = `http://localhost:${tendermintPort}/broadcast_tx_commit`;
+  const tendermintRpcUrl = `http://localhost:${tendermintPort}/broadcast_tx_sync`;
   const result = await axios.get(tendermintRpcUrl, {
     params: {
       tx: txBytes,
