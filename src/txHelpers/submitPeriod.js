41b300ce31e6363dbca20245fe9b31d3ef3aff96
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-04-13 23:26:20 +0200

    Squashed commit of the following:
    commit 812f4667d4ad66a174d826327e0e9688d2749ce5
    Author: Kosta Korenkov <kosta@whoot.me>
    Date:   Fri Apr 12 18:31:28 2019 +0300
    
        fix(period): validator fails to resubmit the very first period (#223)
    
        * fix(period): validator fails to resubmit the very first period
    
        Because the code was tied to the height which may be higher on the next resubmission
    
        * test(submitPeriod): refactor and 100% coverage
    
    commit 7e1dc3624d2a31c74dd01c1f0b95793f3851da8e
    Author: pinkiebell <40266861+pinkiebell@users.noreply.github.com>
    Date:   Fri Apr 12 17:30:08 2019 +0200
    
        Dockerfile: remove install of yarn, already included (#225)

--- src/txHelpers/submitPeriod.js
+++ src/txHelpers/submitPeriod.js
@@ -25,11 +25,11 @@ const mySlotToSubmitFor = (slots, height, bridgeState) => {
   return mySlots.find(slot => slot.id === currentSlotId);
 };
 
-const getPrevPeriodRoot = (period, bridgeState, height) => {
+const getPrevPeriodRoot = (period, bridgeState) => {
   const { lastBlocksRoot, lastPeriodRoot } = bridgeState;
 
+  if (!lastBlocksRoot) return GENESIS; // not submissions yet = first period to be submitted
   if (lastBlocksRoot === period.prevHash) return lastPeriodRoot; // found
-  if (height === 32) return GENESIS; // not found on 32 block = first period to be submitted
   return null; // not found
 };
 
