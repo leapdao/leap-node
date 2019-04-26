721f6a0b72818ef5faa1c0b0f605d698cfc59384
Johann Barbie <johannbarbie@me.com>
2019-04-26 19:49:20 +0200

    added input owner test

--- src/tx/applyTx/checkSpendCond.js
+++ src/tx/applyTx/checkSpendCond.js
@@ -12,8 +12,15 @@ const utils = require('ethereumjs-util');
 const { BigInt, multiply, subtract, lessThan } = require('jsbi-utils');
 const isEqual = require('lodash/isEqual');
 const getColors = require('../../api/methods/getColors');
-const { NFT_COLOR_BASE, NST_COLOR_BASE } = require('../../api/methods/constants');
-const { ERC20_BYTECODE, ERC721_BYTECODE, ERC1948_BYTECODE } = require('./ercBytecode');
+const {
+  NFT_COLOR_BASE,
+  NST_COLOR_BASE,
+} = require('../../api/methods/constants');
+const {
+  ERC20_BYTECODE,
+  ERC721_BYTECODE,
+  ERC1948_BYTECODE,
+} = require('./ercBytecode');
 const { isNFT, isNST } = require('./../../utils');
 
 const { Account } = VM.deps;
@@ -30,7 +37,7 @@ const ERC20_MINT_FUNCSIG = Buffer.from(
 
 const ERC721_MINT_FUNCSIG = Buffer.from(
   '40c10f19000000000000000000000000',
-  'hex',
+  'hex'
 );
 
 const ERC1948_MINT_FUNCSIG = Buffer.from(
@@ -69,7 +76,7 @@ const FIXED_GAS_PRICE = BigInt(142);
 
 function setAccount(account, address, stateManager) {
   return new Promise((resolve, reject) => {
-    stateManager.putAccount(address, account, (err) => {
+    stateManager.putAccount(address, account, err => {
       if (err) {
         return reject(err);
       }
@@ -80,7 +87,7 @@ function setAccount(account, address, stateManager) {
 
 function setAccountCode(code, address, stateManager) {
   return new Promise((resolve, reject) => {
-    stateManager.putContractCode(address, code, (err) => {
+    stateManager.putContractCode(address, code, err => {
       if (err) {
         return reject(err);
       }
@@ -135,7 +142,11 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
 
   addColors(colorMap, await getColors(bridgeState, false), 0);
   addColors(colorMap, await getColors(bridgeState, true), NFT_COLOR_BASE);
-  addColors(colorMap, await getColors(bridgeState, false, true), NST_COLOR_BASE);
+  addColors(
+    colorMap,
+    await getColors(bridgeState, false, true),
+    NST_COLOR_BASE
+  );
 
   const toMint = [];
   const LEAPTokenColor = 0;
@@ -205,33 +216,72 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
     let allowance;
 
     if (!spendingIsOwner) {
+      if (unspent.address !== input.signer) {
+        throw new Error(
+          `output owner ${unspent.address} unequal input signer: ${
+            input.signer
+          }`
+        );
+      }
       allowance = {};
     }
 
     if (isNST(unspent.color)) {
-      callData = Buffer.concat([ERC1948_MINT_FUNCSIG, addrBuf, tokenValueBuf, utils.toBuffer(unspent.data)]);
+      callData = Buffer.concat([
+        ERC1948_MINT_FUNCSIG,
+        addrBuf,
+        tokenValueBuf,
+        utils.toBuffer(unspent.data),
+      ]);
       bytecode = ERC1948_BYTECODE;
 
       if (allowance) {
-        allowance = { from: addrBuf, callData: Buffer.concat([ERC721_APPROVE_FUNCSIG, spendingAddrBuf, tokenValueBuf]) };
+        allowance = {
+          from: addrBuf,
+          callData: Buffer.concat([
+            ERC721_APPROVE_FUNCSIG,
+            spendingAddrBuf,
+            tokenValueBuf,
+          ]),
+        };
       }
     } else if (isNFT(unspent.color)) {
       callData = Buffer.concat([ERC721_MINT_FUNCSIG, addrBuf, tokenValueBuf]);
       bytecode = ERC721_BYTECODE;
 
       if (allowance) {
-        allowance = { from: addrBuf, callData: Buffer.concat([ERC721_APPROVE_FUNCSIG, spendingAddrBuf, tokenValueBuf]) };
+        allowance = {
+          from: addrBuf,
+          callData: Buffer.concat([
+            ERC721_APPROVE_FUNCSIG,
+            spendingAddrBuf,
+            tokenValueBuf,
+          ]),
+        };
       }
     } else {
       callData = Buffer.concat([ERC20_MINT_FUNCSIG, addrBuf, tokenValueBuf]);
       bytecode = ERC20_BYTECODE;
 
       if (allowance) {
-        allowance = { from: addrBuf, callData: Buffer.concat([ERC20_INCREASE_ALLOWANCE_FUNCSIG, spendingAddrBuf, tokenValueBuf]) };
+        allowance = {
+          from: addrBuf,
+          callData: Buffer.concat([
+            ERC20_INCREASE_ALLOWANCE_FUNCSIG,
+            spendingAddrBuf,
+            tokenValueBuf,
+          ]),
+        };
       }
     }
 
-    toMint.push({ contractAddr, callData, bytecode, color: unspent.color, allowance });
+    toMint.push({
+      contractAddr,
+      callData,
+      bytecode,
+      color: unspent.color,
+      allowance,
+    });
   }
 
   // creating a new VM instance
@@ -287,12 +337,16 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
       }
 
       // eslint-disable-next-line no-await-in-loop
-      await runTx(vm, {
-        nonce,
-        gasLimit: GAS_LIMIT_HEX,
-        to: obj.contractAddr,
-        data: obj.allowance.callData,
-      }, obj.allowance.from);
+      await runTx(
+        vm,
+        {
+          nonce,
+          gasLimit: GAS_LIMIT_HEX,
+          to: obj.contractAddr,
+          data: obj.allowance.callData,
+        },
+        obj.allowance.from
+      );
 
       // update nonce
       nonces[owner] = nonce + 1;
@@ -300,7 +354,7 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
   }
 
   // need to commit to trie, needs a checkpoint first 🤪
-  await new Promise((resolve) => {
+  await new Promise(resolve => {
     vm.stateManager.checkpoint(() => {
       vm.stateManager.commit(() => {
         resolve();
@@ -335,12 +389,7 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
       const nstToData = `0x${data.slice(32, 64).toString('hex')}`;
 
       logOuts.push(
-        new Output(
-          BigInt(nstTokenId),
-          spendingAddress,
-          originColor,
-          nstToData
-        )
+        new Output(BigInt(nstTokenId), spendingAddress, originColor, nstToData)
       );
       return;
     }
@@ -356,8 +405,9 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
       // toAddr = `0x${toAddr.toString('hex')}`;
       // }
       // ? ERC721(tokenId) : ERC20(transferAmount)
-      const transferAmount =
-        isNFT(originColor) ? BigInt(`0x${topics[3].toString('hex')}`) : BigInt(`0x${data.toString('hex')}`, 16);
+      const transferAmount = isNFT(originColor)
+        ? BigInt(`0x${topics[3].toString('hex')}`)
+        : BigInt(`0x${data.toString('hex')}`, 16);
 
       logOuts.push(new Output(transferAmount, toAddr, originColor));
     }
@@ -379,14 +429,25 @@ module.exports = async (state, tx, bridgeState, nodeConfig = {}) => {
     );
   }
 
-  const gasChange = subtract(BigInt(spendingInputUnspent.value), multiply(gasPrice, gasUsed));
+  const gasChange = subtract(
+    BigInt(spendingInputUnspent.value),
+    multiply(gasPrice, gasUsed)
+  );
 
   if (lessThan(gasChange, BigInt(0))) {
-    throw new Error('Not enough input for spending condition to cover gas costs');
+    throw new Error(
+      'Not enough input for spending condition to cover gas costs'
+    );
   }
 
   // Now return the leftovers
-  logOuts.push(new Output(gasChange, spendingInputUnspent.address, spendingInputUnspent.color));
+  logOuts.push(
+    new Output(
+      gasChange,
+      spendingInputUnspent.address,
+      spendingInputUnspent.color
+    )
+  );
 
   // TODO: compact logOuts
   if (!isEqual(tx.outputs, logOuts)) {
