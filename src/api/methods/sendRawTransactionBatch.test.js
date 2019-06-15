d92deab42c2ef09c6e8aecea1b10ff5273543b20
Kosta Korenkov <kosta@whoot.me>
2019-06-15 12:09:36 +0300

    chore: workaround jest warning in tests

--- src/api/methods/sendRawTransactionBatch.test.js
+++ src/api/methods/sendRawTransactionBatch.test.js
@@ -7,6 +7,8 @@ describe('sendRawTransactionBatch', () => {
   test('should return number of transactions', async () => {
     const txs = [Tx.deposit(0, 100, A1, 0)];
     const res = await sendRawTransactionBatch(0, txs);
+    // crotch for jest not to complain on setTimeout. TODO: make it better
+    await new Promise(resolve => setTimeout(resolve, 1000));
     expect(res).toBe(txs.length);
   });
 });
