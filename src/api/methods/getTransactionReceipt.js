e15e904687d94d98b95c8879230757d0b57c807c
Kosta Korenkov <kosta@whoot.me>
2019-08-28 22:07:56 +0300

    feat: return proper transaction receipt logs

--- src/api/methods/getTransactionReceipt.js
+++ src/api/methods/getTransactionReceipt.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the Mozilla Public License Version 2.0
  * found in the LICENSE file in the root directory of this source tree.
  */
+const { bufferToHex } = require('ethereumjs-util');
 const { Tx } = require('leap-core');
 const getPrevTx = require('./utils/getPrevTx');
 
@@ -17,18 +18,32 @@ module.exports = async (db, hash) => {
 
   const prevTx = await getPrevTx(db, tx);
 
+  const blockNumber = `0x${height.toString(16)}`;
+
+  const txLogs = (logs || []).map(log => ({
+    transactionLogIndex: 0,
+    transactionIndex: txPos,
+    blockNumber,
+    transactionHash: hash,
+    address: bufferToHex(Buffer.from(log[0])),
+    topics: log[1].map(e => bufferToHex(Buffer.from(e))),
+    data: bufferToHex(Buffer.from(log[2])),
+    logIndex: 0,
+    blockHash,
+  }));
+
   return {
     transactionHash: hash,
     transactionIndex: txPos,
     blockHash,
-    blockNumber: `0x${height.toString(16)}`,
+    blockNumber,
     from: tx.from(prevTx),
     to: tx.to(),
     raw: tx.hex(),
     cumulativeGasUsed: '0x0',
     gasUsed: '0x0',
     contractAddress: null,
-    logs: logs || [],
+    logs: txLogs,
     logsBloom: '0x',
     status: '0x1',
   };
