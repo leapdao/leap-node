2927a4f5e8a078b7392b1bbcded8117ef0606ba0
vrde <agranzot@gmail.com>
2019-11-08 15:12:10 +0100

    Use network settings specified in the config (#353)
    * Use network settings specified in the config
    
    Problem: the parameter `rootNetwork` contained in the configuration is
    ignored, while `rootNetworkId` is used to match the `rootNetwork` from
    `./src/utils/networks.js`. This prevents a node operator to connect to
    the endpoint they want.
    
    Solution: use `rootNetwork` and remove `./src/utils/networks.js`.
    
    Fix: #347
    
    * Address code review comments

--- src/utils/readConfig.test.js
+++ src/utils/readConfig.test.js
@@ -12,6 +12,8 @@ describe('readConfig', () => {
       JSON.stringify({
         exitHandlerAddr: '0x000',
         network: 'testnet',
+        rootNetwork: 'http://localhost',
+        rootNetworkId: 5777,
       })
     );
   });
@@ -20,35 +22,41 @@ describe('readConfig', () => {
   const readConfig = require('./readConfig');
 
   test('from file', async () => {
-    Web3.__setMethodMock('getConfig');
+    Web3.__setMethodMock('eth.net.getId', () => 5777);
 
     const result = await readConfig('./config.json');
     expect(result).toEqual({
       exitHandlerAddr: '0x000',
       network: 'testnet',
+      rootNetwork: 'http://localhost',
+      rootNetworkId: 5777,
       eventsDelay: 0,
       bridgeDelay: 0,
     });
   });
 
   test('fetch node config', async () => {
-    Web3.__setMethodMock('getConfig', {
+    Web3.__setMethodMock('getConfig', () => ({
       exitHandlerAddr: '0x000',
       network: 'testnet',
-    });
+      rootNetwork: 'http://localhost',
+      rootNetworkId: 5777,
+    }));
     const result = await readConfig('https://tesnet.leapdao.org');
     expect(result).toEqual({
       exitHandlerAddr: '0x000',
       network: 'testnet',
+      rootNetwork: 'http://localhost',
+      rootNetworkId: 5777,
       eventsDelay: 0,
       bridgeDelay: 0,
     });
   });
 
   test('without bridge address', async () => {
-    Web3.__setMethodMock('getConfig', {
+    Web3.__setMethodMock('getConfig', () => ({
       network: 'testnet',
-    });
+    }));
     let error;
     try {
       await readConfig('https://tesnet.leapdao.org');
@@ -59,16 +67,20 @@ describe('readConfig', () => {
   });
 
   test("append donor's peer", async () => {
-    Web3.__setMethodMock('getConfig', {
+    Web3.__setMethodMock('getConfig', () => ({
       exitHandlerAddr: '0x000',
       network: 'testnet',
+      rootNetwork: 'http://localhost',
+      rootNetworkId: 5777,
       p2pPort: 41000,
       nodeId: 'blahblah',
-    });
+    }));
     const result = await readConfig('https://tesnet.leapdao.org');
     expect(result).toEqual({
       exitHandlerAddr: '0x000',
       network: 'testnet',
+      rootNetwork: 'http://localhost',
+      rootNetworkId: 5777,
       peers: ['blahblah@tesnet.leapdao.org:41000'],
       eventsDelay: 0,
       bridgeDelay: 0,
