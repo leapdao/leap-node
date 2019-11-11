8fed8d34e279cb3faa4d639549fac56e3dc64361
Alex Lunyov <isuntc@gmail.com>
2019-11-12 00:42:32 +0800

    Update src/flags/index.js
    Co-Authored-By: Kosta Korenkov <kosta@whoot.me>

--- src/flags/index.js
+++ src/flags/index.js
@@ -1,5 +1,9 @@
 const flagsFactory = require('./flagsFactory');
 
-const FLAGS = ['spend_cond_stricter_rules', 'spend_cond_new_bytecode'];
+// when adding a flag, add a link to the issue/PR describing the reason for the change
+const FLAGS = [
+  'spend_cond_stricter_rules', // https://github.com/leapdao/leap-node/pull/303
+  'spend_cond_new_bytecode' // https://github.com/leapdao/leap-node/pull/292
+];
 
 module.exports = flagsFactory(FLAGS);
