f20e31f76d33142d3e307c0359b7250c93171aaa
vrde <agranzot@gmail.com>
2019-12-16 17:27:03 +0100

    Add more comments on defaults

--- src/heartbeat/defaults.js
+++ src/heartbeat/defaults.js
@@ -1,5 +1,14 @@
 module.exports = {
-  period: 60 * 1000,
-  periodOnError: 5 * 1000,
+  // The color must be provided by the configuration
+  color: undefined,
+
+  // Filter out the heartbeat NFT from `plasma_getUnspent`
   filter: true,
+
+  // In milliseconds, time between heartbeats
+  period: 60 * 1000,
+
+  // In milliseconds, time to wait before sending a new heartbeat
+  // if the previous one failed
+  periodOnError: 5 * 1000,
 };
